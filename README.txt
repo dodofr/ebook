38d5311e2f5d1b857f4923d70215040c345575a2
Julie et Dorian Chopin <chopin.dorian@gmail.com>
2024-11-03 17:55:40 +0400

    gestion series

--- README.txt
+++ README.txt
@@ -1,1 +1,14 @@
-Ajouter le dossier uploads\ebooks
+bdd : https://dash.filess.io
+
+Ajouter le dossier uploads\ebooks 
+
+Dans insomnia mettre multipart puis bien choisir file que nomme file comme app.post("/api/ebooks",upload.single('file'), createEbookcontroller)
+structure dans l'onglet Multipart Form :
+file : choisissez le fichier à télécharger.
+titre : "Mon nouvel ebook".
+description : "Description de l'ebook".
+note : 9.
+commentaire : "Très bon ebook".
+auteur : "Auteur Exemple".
+serieNom : star wars // si deja la alors rajoute
+categorieIds : [1, 2].
--- src/controllers/ebookcontroller.js
+++ src/controllers/ebookcontroller.js
@@ -1,19 +1,58 @@
-const {Ebook, Categorie} = require('../db/sequelize');
-async function createEbookcontroller (req, res) {
+// const {Ebook, Categorie} = require('../db/sequelize');
+// async function createEbookcontroller (req, res) {
+//     try {
+//         const { titre, description, note, commentaire, auteur, serieId, categorieIds } = req.body;
+//         const lienTelechargement = req.file ? `../../uploads/ebooks/${req.file.filename}` : null;
+
+//         const newEbook = await Ebook.create({
+//             titre,
+//             description,
+//             note,
+//             commentaire,
+//             auteur,
+//             serieId,
+//             lienTelechargement,
+//         });
+
+//         if (categorieIds && categorieIds.length > 0) {
+//             const categories = await Categorie.findAll({ where: { id: categorieIds } });
+//             await newEbook.addCategories(categories);
+//         }
+
+//         res.status(201).json(newEbook);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// module.exports = {
+//     createEbookcontroller
+// }
+const { Ebook, Serie, Categorie } = require('../db/sequelize');
+
+async function createEbookcontroller(req, res) {
     try {
-        const { titre, description, note, commentaire, auteur, serieId, categorieIds } = req.body;
+        const { titre, description, note, commentaire, auteur, serieNom, categorieIds } = req.body;
         const lienTelechargement = req.file ? `../../uploads/ebooks/${req.file.filename}` : null;
 
+        // Vérifiez si la série existe, sinon créez-la
+        let serie = await Serie.findOne({ where: { nom: serieNom } });
+        if (!serie) {
+            serie = await Serie.create({ nom: serieNom });
+        }
+
+        // Créez l'ebook en utilisant l'ID de la série
         const newEbook = await Ebook.create({
             titre,
             description,
             note,
             commentaire,
             auteur,
-            serieId,
+            serieId: serie.id, // Utilisez l'ID de la série
             lienTelechargement,
         });
 
+        // Associez les catégories si fournies
         if (categorieIds && categorieIds.length > 0) {
             const categories = await Categorie.findAll({ where: { id: categorieIds } });
             await newEbook.addCategories(categories);
@@ -23,8 +62,8 @@ async function createEbookcontroller (req, res) {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-};
+}
 
 module.exports = {
     createEbookcontroller
-}
+};
